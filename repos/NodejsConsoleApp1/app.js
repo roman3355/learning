313ed5e075db97a4e8d0aa253f54d3cc2eff4d8f
Roman <roman335544@gmail.com>
2022-09-25 21:44:10 +0700

    first commit

---
+++ repos/NodejsConsoleApp1/app.js
@@ -0,0 +1,44 @@
+/* let adm1=prompt('Ваше имя?', '');
+if (adm2=='Роман') {
+    let password=prompt('Пароль', '') {
+        if (password==123) {
+            alert('Welcome, sir');
+        } else if (password=='' || password==null) {
+            alert('Отмена');
+        } else {
+            alert('Пароль не введён');
+        }
+    } else if (adm1=='' || adm1==null) {
+        aleer('Имя не введено');
+} else {
+    alert('Отказ во входе');
+}
+}
+
+const COLOR_CASE = '';
+let color_case = '';
+
+const obj = {
+    name: 'Abc',
+    age: 12364,
+    color: 'gray-green'
+};
+console.log(obj['color']);
+
+const massive = ['abc', 'bcd', 3, 'efg', 15];
+console.log(massive[3]);
+
+const massive1 = ['abc', 'bcd', 3, 'efg', 15];
+console.log(massive1.length); // количество элементов в массиве; */
+
+const answers = [];
+answers[0] = prompt('Сколько вам лет?', '');
+answers[1] = prompt('Как ваше имя?', '');
+answers[2] = prompt('Как ваша фамилия?', '');
+document.write(answers);
+
+function sayHello() {
+    console.log('I say hell0');
+}
+sayHello();
+
---
+++ repos/NodejsConsoleApp1/newFile.js
@@ -0,0 +1,4 @@
+console.log('arr' + ' - object');
+console.log(4+' - number');
+console.log('sfdasdf' + 'sdfa');
+console.log(4 + +'5');
